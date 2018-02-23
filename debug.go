f6022c9e39e2af7c42b018d5b076c47715910b29
Geoff Catlin <gcatlin@gmail.com>
2018-02-22 20:42:24 -0500

    First commit

---
+++ debug.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func printValue(value Value) {
+	fmt.Printf("%g", value)
+}
+
+func constantInstruction(name string, chunk *Chunk, offset int) int {
+	byte0 := int(chunk.code[offset+1])
+	constant := byte0
+	fmt.Printf("%-16s %4d '", name, constant)
+	printValue(chunk.constants[constant])
+	fmt.Printf("'\n")
+	return offset + 2
+}
+
+func constantXInstruction(name string, chunk *Chunk, offset int) int {
+	byte0 := int(chunk.code[offset+1])
+	byte1 := int(chunk.code[offset+2])
+	byte2 := int(chunk.code[offset+3])
+	constant := byte0 | (byte1 << 8) | (byte2 << 16)
+	fmt.Printf("%-16s %4d '", name, constant)
+	printValue(chunk.constants[constant])
+	fmt.Printf("'\n")
+	return offset + 4
+}
+
+func simpleInstruction(name string, offset int) int {
+	fmt.Printf("%-16s\n", name)
+	return offset + 1
+}
+
+func unknownInstruction(instr byte, offset int) int {
+	fmt.Printf("Unknown opcode: %d\n", instr)
+	return offset + 1
+}
+
+func (c *Chunk) disassemble(name string) {
+	fmt.Printf("=== %s ===\n", name)
+	fmt.Printf("OFFSET B0 B1 B2 B3 LINE   OPCODE\n")
+	fmt.Printf("------ -- -- -- -- -----  ----------------\n")
+	for i, max := 0, len(c.code); i < max; {
+		i = c.disassembleInstruction(i)
+	}
+}
+
+func (c *Chunk) disassembleInstruction(offset int) int {
+	line := c.getLine(offset)
+	instr := c.code[offset]
+	size := InstrSize[instr]
+
+	// Instruction bytes
+	fmt.Printf("%06d ", offset)
+	for i := 0; i < size; i++ {
+		fmt.Printf(HEX+" ", c.code[offset+i])
+	}
+	for i := size; i < 4; i++ {
+		fmt.Printf("   ")
+	}
+
+	// Line numbers
+	if offset > 0 && line == c.getLine(offset-1) {
+		fmt.Printf("    |  ")
+	} else {
+		fmt.Printf("%5d  ", line)
+	}
+
+	switch instr {
+	case OpConstant:
+		constantInstruction("OP_CONSTANT", c, offset)
+	case OpConstantX:
+		constantInstruction("OP_CONSTANT_X", c, offset)
+	case OpReturn:
+		simpleInstruction("OP_RETURN", offset)
+	default:
+		unknownInstruction(instr, offset)
+	}
+	return offset + size
+}
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+const HEX = "%02X"
+
+const (
+	OpConstant byte = iota
+	OpConstantX
+	OpReturn
+)
+
+var InstrSize = []int{
+	OpConstant:  2,
+	OpConstantX: 4,
+	OpReturn:    1,
+}
+
+type Value float64
+
+type Chunk struct {
+	code      []byte
+	constants []Value
+	lines     []int
+	offsets   []int
+}
+
+func NewChunk() *Chunk {
+	return &Chunk{
+		code:      make([]byte, 0, 16),
+		constants: make([]Value, 0, 4),
+		lines:     make([]int, 0, 4),
+		offsets:   make([]int, 0, 4),
+	}
+}
+
+func (c *Chunk) addConstant(v Value) int {
+	c.constants = append(c.constants, v)
+	return len(c.constants) - 1
+}
+
+func (c *Chunk) getLine(offset int) int {
+	var mid int
+	found := false
+	low := 0
+	high := len(c.lines) - 1
+	for low <= high {
+		mid = (low + high) / 2
+		if offset < c.offsets[mid] {
+			high = mid - 1
+		} else if offset > c.offsets[mid] {
+			low = mid + 1
+		} else {
+			found = true
+			break
+		}
+	}
+	if !found && offset <= c.offsets[mid] {
+		mid--
+	}
+	return c.lines[mid]
+}
+
+func (c *Chunk) write(b byte, line int) {
+	n := len(c.lines)
+	if n == 0 || c.lines[n-1] != line {
+		c.lines = append(c.lines, line)
+		c.offsets = append(c.offsets, len(c.code))
+	}
+	c.code = append(c.code, b)
+}
+
+func (c *Chunk) writeConstant(v Value, line int) {
+	constant := c.addConstant(v)
+	// if len(c.constants) <= 1 {
+	if len(c.constants) <= 0xFF {
+		c.write(OpConstant, line)
+		c.write(byte(constant), line)
+		return
+	}
+	c.write(OpConstantX, line)
+	c.write(byte(constant&0xFF), line)
+	c.write(byte((constant>>8)&0xFF), line)
+	c.write(byte((constant>>16)&0xFF), line)
+}
+
+func main() {
+	c := NewChunk()
+	c.writeConstant(1.2, 123)
+	c.writeConstant(3.4, 124)
+	c.writeConstant(5.6, 124)
+	c.write(OpReturn, 125)
+	c.disassemble("test chunk")
+}
